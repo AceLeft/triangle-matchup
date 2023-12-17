1c1066b6accb0ab62488287c604fde0175a77d61
Brooke Wright <brookerwright2@gmail.com>
2023-12-17 16:15:57 -0500

    Set a Target's sprite texture to a premade triangle set
    Note that the premade images are way too big right now

--- project/common/congruent.gd
+++ project/common/congruent.gd
@@ -24,3 +24,16 @@ func modulate_to_correct_color(sprite : Sprite2D, type_num : int) -> void:
 		sprite.set_modulate(Color("4a7d45"))
 	if type_num == Types.NOT:
 		sprite.set_modulate(Color("6a6f73"))
+
+
+func set_sprite_to_random_triangles(sprite :Sprite2D, desired_type : String) -> void:
+	# It is expected that the images follow these conventions:
+	# 1. Begin with a lowercase version of their type
+	# 2. The name ends in a number 1-4
+	# 3. Is a .png
+	# 4. They are located inside the target folder
+	var path := "res://target/" + desired_type.to_lower() + str(randi_range(1,4)) + ".png"
+	var image := Image.load_from_file(path)
+	var texture := ImageTexture.create_from_image(image)
+	sprite.texture = texture
+	
--- project/target/target.gd
+++ project/target/target.gd
@@ -3,21 +3,15 @@ extends RigidBody2D
 
 
 @onready var type
-@onready var _ball_sprite : Sprite2D = $Ball
+@onready var _sprite : Sprite2D = $Triangles
 
 
 func _ready() -> void:
-	# Only give a random color if it does not already have one
+	# Only give a random type if it does not already have one
 	if type == null:
-		var chosen_color = randi() % Congruent.Types.size()
-		type = Congruent.Types.find_key(chosen_color)
-		Congruent.modulate_to_correct_color(_ball_sprite, chosen_color)
-
-
-func set_color(color) -> void:
-	type = color
-	# Cannot remove $Ball because _ready() may not have been called yet
-	Congruent.modulate_to_correct_color($Ball, Congruent.Types.get(color))
+		var chosen_type = randi() % Congruent.Types.size()
+		type = Congruent.Types.find_key(chosen_type)
+		Congruent.set_sprite_to_random_triangles(_sprite, type)
 
 
 func _on_ball_stopper_body_entered(body):
