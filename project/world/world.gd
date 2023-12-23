6844b1341e4ec2db8325565a2fd52b4097accb37
Brooke Wright <brookerwright2@gmail.com>
2023-12-23 10:48:13 -0500

    Fix the fail condition not happening

--- project/world/world.gd
+++ project/world/world.gd
@@ -28,11 +28,6 @@ func _on_target_manager_targets_gone() -> void:
 	_go_to_end_screen()
 
 
-func _on_fail_line_area_area_entered(_area) -> void:
-	EndCondition.won = false
-	_go_to_end_screen()
-
-
 func _go_to_end_screen() -> void:
 	EndCondition.final_score = _score_keeper.get_score()
 	get_tree().call_deferred("change_scene_to_file", "res://ui/end_screen.tscn")
@@ -42,5 +37,12 @@ func _on_target_manager_move_down_requested() -> void:
 	_check_launcher = true
 
 
-func _on_target_manager_target_deleted():
+func _on_target_manager_target_deleted() -> void:
 	_score_keeper.increase_score_by(_SCORE_INCREASE_AMOUNT)
+
+
+func _on_fail_line_area_area_entered(area : Area2D) -> void:
+	var area_parent = area.get_parent()
+	if is_instance_of(area_parent, Target):
+		EndCondition.won = false
+		_go_to_end_screen()
