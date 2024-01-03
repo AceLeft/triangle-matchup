395a726da7d1c5189491391b5ad8deb2a5d2423a
Brooke Wright <brookerwright2@gmail.com>
2024-01-03 15:06:11 -0500

    Change colors and remove HL

--- project/common/congruent.gd
+++ project/common/congruent.gd
@@ -6,24 +6,21 @@ enum Types {
 	SAS,
 	AAS,
 	ASA,
-	HL,
 	NOT,
 }
 
 
 func modulate_to_correct_color(sprite : Sprite2D, type_num : int) -> void:
 	if type_num == Types.SSS:
-		sprite.set_modulate(Color("ff8426"))
+		sprite.set_modulate(Color("01b1e1"))
 	if type_num == Types.SAS:
-		sprite.set_modulate(Color("68AED4"))
+		sprite.set_modulate(Color("f6a000"))
 	if type_num == Types.AAS:
-		sprite.set_modulate(Color("94216a"))
+		sprite.set_modulate(Color("9665d9"))
 	if type_num == Types.ASA:
-		sprite.set_modulate(Color("ff2674"))
-	if type_num == Types.HL:
-		sprite.set_modulate(Color("4a7d45"))
+		sprite.set_modulate(Color("f773a2"))
 	if type_num == Types.NOT:
-		sprite.set_modulate(Color("6a6f73"))
+		sprite.set_modulate(Color("f4e8bf"))
 
 
 func set_sprite_to_random_triangles(sprite :Sprite2D, desired_type : String) -> void:
